824cd8f3ac7b2546d3118a9947689f17c94798dd
zhengchanglin <yh.lvdousha@gmail.com>
2018-03-20 22:48:34 +0800

    原生SQL查询

--- stonecloud-web-boot/src/main/java/com/example/controller/ExampleController.java
+++ stonecloud-web-boot/src/main/java/com/example/controller/ExampleController.java
@@ -1,10 +1,17 @@
 package com.example.controller;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.mapper.ApplicationServerMapper;
+import com.example.model.AdminUser;
 import com.example.model.ApplicationServer;
 
 @RestController
@@ -12,12 +19,21 @@ public class ExampleController {
 
 	@Autowired
 	private ApplicationServerMapper applicationServerMapper;
+	@Autowired
+	@Qualifier("entityManagerPrimary")
+	private EntityManager entityManager;
 	
 	@RequestMapping("/")
 	String home() {
-		ApplicationServer as = new ApplicationServer();
-		as.setServerName("test");
-		applicationServerMapper.save(as);
+		return "Hello World!";
+	}
+	
+	@RequestMapping("/nativeQuery")
+	String nativeQuery() {
+		Query query = entityManager.createNativeQuery("select * from adminuser", AdminUser.class);
+		@SuppressWarnings("unchecked")
+		List<AdminUser> adminUsers = query.getResultList();
+		System.out.println(adminUsers.get(0).getPassword());
 		return "Hello World!";
 	}
 	
