4455a212a5167da9de205dcbe26f8a022d0c6ac8
zhengchanglin <yh.lvdousha@gmail.com>
2018-05-27 20:57:57 +0800

    logback——日志配置

--- stonecloud-web-boot/src/main/java/com/example/controller/ExampleController.java
+++ stonecloud-web-boot/src/main/java/com/example/controller/ExampleController.java
@@ -5,17 +5,22 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.config.BasicConfigBean;
+import com.example.config.DataSourceConfig;
 import com.example.model.AdminUser;
 
 @RestController
 public class ExampleController {
 
+	private Logger log = LoggerFactory.getLogger(ExampleController.class);
+	
 	@Autowired
 	@Qualifier("entityManagerPrimary")
 	private EntityManager entityManager;
@@ -24,6 +29,7 @@ public class ExampleController {
 	
 	@RequestMapping("/")
 	String home() {
+		log.info("HOME");
 		return basicConfig.getName();
 	}
 	
