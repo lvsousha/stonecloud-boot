a99d9d2fcb22fc5215bddedde9f16f84f37e72a5
郑昌林 <zhengchanglin@bmsoft.com>
2019-09-19 20:54:17 +0800

    回避仲裁员

--- stonecloud-web-boot/src/main/java/com/stone/controller/ProcessController.java
+++ stonecloud-web-boot/src/main/java/com/stone/controller/ProcessController.java
@@ -1,13 +1,20 @@
 package com.stone.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import org.flowable.engine.RepositoryService;
 import org.flowable.engine.RuntimeService;
 import org.flowable.engine.TaskService;
+import org.flowable.engine.repository.ProcessDefinition;
 import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.task.api.Task;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -17,11 +24,19 @@ public class ProcessController {
     @Autowired
     private RuntimeService runtimeService;
     @Autowired
+    private RepositoryService repositoryService;
+    @Autowired
     private TaskService taskService;
 
-    @RequestMapping(value = "atart")
+    @RequestMapping(value = "test")
     @ResponseBody
-    public String atart() {
+    public void test(@RequestBody JSONObject param) {
+      log.info("启动回避仲裁员" + param.getString("id"));
+    }
+
+    @RequestMapping(value = "start")
+    @ResponseBody
+    public String start() {
       log.info("启动回避仲裁员");
         //启动流程
         HashMap<String, Object> map = new HashMap<>();
@@ -30,9 +45,28 @@ public class ProcessController {
         return "提交成功.流程Id为：" + processInstance.getId();
     }
 
+    @RequestMapping(value = "/list")
+    @ResponseBody
+    public List<JSONObject> list() {
+      List<Task> tasks = taskService.createTaskQuery().list();
+      List<JSONObject> list = new ArrayList<>();
+      for(Task task : tasks) {
+        JSONObject model = new JSONObject();
+        ProcessDefinition processDefinition= repositoryService.createProcessDefinitionQuery().processDefinitionId(task.getProcessDefinitionId()).singleResult();
+        model.put("process", processDefinition.getName());
+        model.put("id", task.getId());
+        model.put("name", task.getName());
+        model.put("def", task.getTaskDefinitionKey());
+        list.add(model);
+      }
+        return list;
+    }
+
     @RequestMapping(value = "/audit")
     @ResponseBody
-    public Object audit(String operate, String taskId) {
+    public Object audit(@RequestBody JSONObject param) {
+      String taskId = param.getString("taskId");
+      String operate = param.getString("operate");
       taskService.setVariableLocal(taskId, "operateType", operate);
       taskService.setVariable(taskId, "operate", operate);
       taskService.complete(taskId);
--- stonecloud-web-boot/src/main/resources/static/app.js
+++ stonecloud-web-boot/src/main/resources/static/app.js
@@ -1,6 +1,56 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    tasks: [{
+      id: "id1",
+      name: "name1"
+    }, {
+      id: "id2",
+      name: "name2"
+    }]
+  },
+  methods: {
+    test: function () {
+      axios.post('process/test', {
+        id: 'Fred'
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    },
+    start: function () {
+      axios.post('process/start')
+      .then(function (response) {
+        list();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    },
+    audit: function (taskId, operate) {
+      axios.post('process/audit', {
+        taskId: taskId,
+        operate: operate
+      })
+      .then(function (response) {
+        app.list();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    },
+    list: function () {
+      axios.post('process/list')
+      .then(function (response) {
+        app.tasks = response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
   }
-})
+});
+app.list();
